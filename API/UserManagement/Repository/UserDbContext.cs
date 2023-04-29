ad859acf629c1f393d9dbd35dd9a21e23e1eae79
Korovetskii, Andrii <andrii.korovetskyi@gmail.com>
2023-04-29 13:31:39 +0200

    add initial migration

--- API/UserManagement/Repository/UserDbContext.cs
+++ API/UserManagement/Repository/UserDbContext.cs
@@ -6,4 +6,9 @@ namespace UserManagementEF;
 public class UserDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=localhost;Database=CarService.UserManagement;TrustServerCertificate=True;User=sa;Password=Welcome1$;");
+    }
 }
